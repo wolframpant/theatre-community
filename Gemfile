a879bc556c51b67a13ade2fff270fcab96ea6deb
Ellen W <llnwlfsn@gmail.com>
2014-11-26 23:19:28 +0000

    seeded database

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'rails', '4.0.9'
 
 gem 'haml-rails'
 
+gem 'faker'
+
 gem 'bootstrap-sass', '~> 3.1.1'
 
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Impressions
+50.times do
+  Impression.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+  )
+end
+impressions = Impression.all
+
+# Create Comments
+100.times do
+  Comment.create!(
+    impression: impressions.sample,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Impression.count} impressions created"
+puts "#{Comment.count} comments created"
