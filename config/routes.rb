581a48270ed317bb93381c41a2e6b6cd9dcbf591
Ellen W <llnwlfsn@gmail.com>
2014-11-26 22:00:51 +0000

    created README and welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 TheatreCommunity::Application.routes.draw do
-root "application#hello"
+  get "welcome/index"
+  get "welcome/about"
+
+  root "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
