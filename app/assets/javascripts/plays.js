89457b8a75e63121997fa00a999bda6ca5d572ba
Ellen <llnwlfsn@gmail.com>
2015-02-09 02:12:20 +0000

    accordion responsive

--- app/assets/javascripts/plays.js
+++ app/assets/javascripts/plays.js
@@ -3,14 +3,14 @@
     $('#accordion > li').hover(
       function () {
         var $this = $(this);
-        $this.stop().animate({'width':'480px'},500);
+        $this.stop().animate({'width':'40%'},500);
         $('.heading',$this).stop(true,true).fadeOut();
         $('.bgDescription',$this).stop(true,true).slideDown(500);
         $('.description',$this).stop(true,true).fadeIn();
       },
       function () {
         var $this = $(this);
-        $this.stop().animate({'width':'70px'},500);
+        $this.stop().animate({'width':'5%'},500);
         $('.heading',$this).stop(true,true).fadeIn();
         $('.description',$this).stop(true,true).fadeOut(500);
         $('.bgDescription',$this).stop(true,true).slideUp(700);
